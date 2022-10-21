54bbf28b2ff55cec00e639e70682b5ac9d290c42
Julian Osborne <subaruforge@gmail.com>
2022-10-20 21:51:27 -0400

    Updated data

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,22 +1,316 @@
 export const DATA = {
-    weapons: {
-        assault_rifles: [],
-        submachine_guns: [],
-        tactical_rifles: [],
-        light_machine_guns: [],
-        sniper_rifles: [],
-        pistols: [],
-        shotguns: [],
-    },
-    attachments: {
-        optic: [],
-        muzzle: [],
-        barrel: [],
-        body: [],
-        underbarrel: [],
-        magazine: [],
-        handle: [],
-        stock: [],
+    {
+    "weapons": {
+        "assault_rifles": [
+            {
+                "id": "alpha",
+                "name": "XM4",
+                "description": "Full-auto assault rifle. Reliable damage with improved fire rate. Fair weapon control when sustaining fire.",
+                "unlock_level": 0,
+                "ammo_capacity": 30,
+                "mags": 3,
+                "stats": {
+                    "firepower": {
+                        "damage": 30,
+                        "effective_damage_range": 45.72,
+                        "fire_rate": 722,
+                        "bullet_velocity": 657,
+                        "armor_damage": null,
+                        "melee_quickness": 1000
+                    },
+                    "speed": {
+                        "movement_speed": 10.26,
+                        "sprinting_move_speed": 14.49,
+                        "shooting_move_speed": 9.16,
+                        "sprint_to_fire_time": 400,
+                        "aim_walking_movement_speed": 4.06,
+                        "aim_down_sight_time": 300
+                    },
+                    "accuracy": {
+                        "vertical_recoil_control": 360,
+                        "horizontal_recoil_control": 30,
+                        "centering_speed": 11,
+                        "idle_sway_control": 6,
+                        "flinch_resistance": 10,
+                        "hip_fire_accuracy": 7.5
+                    },
+                    "ammo": {
+                        "magazine_ammo_capacity": 30,
+                        "reload_quickness": 2.5,
+                        "max_starting_ammo": 90,
+                        "ammo_capacity": 180,
+                        "increased_salvage_drop_rate": false,
+                        "reveal_distance_ads": null,
+                        "increased_equipment_dro_rate": false
+                    }
+                },
+                "attachments": {
+                    "optic": [
+                        "Millstop Reflex",
+                        "Visiontech 2X",
+                        "Kobra Red Dot",
+                        "Quickdot LED",
+                        "Axial Arms 3X",
+                        "Silix Holoscout",
+                        "Microflex LED",
+                        "Hawksmoor",
+                        "Royal & Kross 4X",
+                        "Susat Multizoom",
+                        "Diamondback Reflex",
+                        "AN/PVS-4 Thermal"
+                    ],
+                    "muzzle": [
+                        "Muzzle Brake 5.56",
+                        "Flash Guard 5.56",
+                        "Suppressor", 
+                        "Infantry Compensator",
+                        "Socom Eliminator",
+                        "Agency Suppressor"
+                    ],
+                    "barrel": [
+                        "11.8\" Extended",
+                        "12.5\" Cavalry Lancer",
+                        "13.5\" Reinforced Heavy",
+                        "13.7\" Ranger",
+                        "13.7\" Takedown",
+                        "13.5\" Taskforce"
+                    ],
+                    "body": [
+                        "Steady Aim Laser",
+                        "Mounted Flashlight",
+                        "SOF Target Designator",
+                        "SWAT 5MW Laser Sight",
+                        "Tiger Team Spotlight",
+                        "Ember Sighting Point"
+                    ],
+                    "underbarrel": [
+                        "Foregrip",
+                        "Infiltrator Grip",
+                        "Patrol Grip",
+                        "Bruiser Grip",
+                        "Field Agent Grip",
+                        "SFOD Speed Grip"
+                    ],
+                    "magazine": [
+                        "40 Round",
+                        "Jungle-Style Mag",
+                        "35 Round Speed Mag",
+                        "Stanag 50 RND",
+                        "B-OPS Mini Clamp",
+                        "Salvo 45 RND Fast Mag"
+                    ],
+                    "handle": [
+                        "Speed Tape",
+                        "Dropshot Wrap",
+                        "Field Tape",
+                        "SASR Jungle Grip",
+                        "Serpent Wrap",
+                        "Airborne Elastic Wrap"
+                    ],
+                    "stock": [
+                        "Tactical Stock",
+                        "Wire Stock",
+                        "Buffer Tube",
+                        "SAS Combat Stock",
+                        "Raider Pad"
+                    ]
+                }
+            },
+            {
+                "id": "bravo",
+                "name": "AK-47",
+                "description": "Full-auto assault rifle. High damage with marginally slower fire rate. Excellent stopping power for mid range encounters.",
+                "unlock_level": 0,
+                "ammo_capacity": 30,
+                "mags": 3,
+                "stats": {
+                    "firepower": {
+                        "damage": 38,
+                        "effective_damage_range":38.1,
+                        "fire_rate": 600,
+                        "bullet_velocity": 702,
+                        "armor_damage": null,
+                        "melee_quickness": 1000
+                    },
+                    "speed": {
+                        "movement_speed": 10.26,
+                        "sprinting_move_speed": 14.49,
+                        "shooting_move_speed": 8.92,
+                        "sprint_to_fire_time": 400,
+                        "aim_walking_movement_speed": 4.06,
+                        "aim_down_sight_time": 300
+                    },
+                    "accuracy": {
+                        "vertical_recoil_control": 405,
+                        "horizontal_recoil_control": -84,
+                        "centering_speed": 11,
+                        "idle_sway_control": 20,
+                        "flinch_resistance": 10,
+                        "hip_fire_accuracy": 7.5
+                    },
+                    "ammo": {
+                        "magazine_ammo_capacity": 30,
+                        "reload_quickness": 2.6,
+                        "max_starting_ammo": 90,
+                        "ammo_capacity": 180,
+                        "increased_salvage_drop_rate": false,
+                        "reveal_distance_ads": null,
+                        "increased_equipment_dro_rate": false
+                    }
+                },
+                "attachments": {
+                    "optic": [
+                        "Millstop Reflex",
+                        "Visiontech 2X",
+                        "Kobra Red Dot",
+                        "Quickdot LED",
+                        "Axial Arms 3X",
+                        "Silix Holoscout",
+                        "Microflex LED",
+                        "Hawksmoor",
+                        "Royal & Kross 4X",
+                        "Susat Multizoom",
+                        "Diamondback Reflex",
+                        "Noch Sova Thermal"
+                    ],
+                    "muzzle": [
+                        "Muzzle Brake 7.62",
+                        "Flash Guard 7.62",
+                        "Suppressor", 
+                        "Spetsnaz Compensator",
+                        "KGB Eliminator",
+                        "GRU Suppressor"
+                    ],
+                    "barrel": [
+                        "15.5\" Ultralight",
+                        "16.5\" Cavalry Lancer",
+                        "18.2\" VDV Reinforced",
+                        "20\" Liberator",
+                        "18/2\" Takedown",
+                        "20\" Spetsnaz RPB Barrel"
+                    ],
+                    "body": [
+                        "Steady Aim Laser",
+                        "Mounted Flashlight",
+                        "KGB Target Designator",
+                        "GRU 5MW Laser Sight",
+                        "Tiger Team Spotlight",
+                        "Ember Sighting Point"
+                    ],
+                    "underbarrel": [
+                        "Foregrip",
+                        "Infiltrator Grip",
+                        "Patrol Grip",
+                        "Bruiser Grip",
+                        "Spetsnaz Grip",
+                        "Spetsnaz Speed Grip"
+                    ],
+                    "magazine": [
+                        "40 Round",
+                        "Taped Mags",
+                        "40 Round Speed Mag",
+                        "Bakelite 50 RND",
+                        "GRU Mag Clamp",
+                        "VDV 50 RND Fast Mag"
+                    ],
+                    "handle": [
+                        "Speed Tape",
+                        "Dropshot Wrap",
+                        "Field Tape",
+                        "Spetsnaz Field Grip",
+                        "Serpent Wrap",
+                        "GRU Elastic Wrap"
+                    ],
+                    "stock": [
+                        "Tactical Stock",
+                        "Wire Stock",
+                        "Buffer Tube",
+                        "Duster Stock",
+                        "No Stock",
+                        "Spetsnaz PKM Stock",
+                        "KGB Skeleton Stock"
+                    ]
+                }
+            }
+        ],
+        "submachine_guns": [],
+        "tactical_rifles": [],
+        "light_machine_gune": [],
+        "sniper_rifles": [],
+        "pistols": [],
+        "shotguns": [],
+        "attachments": {
+            "optic": [
+                {
+                    "tag": "Millstop Reflex",
+                    "zoom": 1.37
+                },
+                {
+                    "tag": "Visiontech 2X",
+                    "zoom": 2
+                }
+            ],
+            "muzzles": [
+                {
+                    "tag": "Muzzle Brake 5.56",
+                    "description": "Reverse venting redirects gases to the sides to reduce muzzle rise.",
+                    "pros": [
+                        {
+                            "tag": "vertical_recoil_control",
+                            "percent": 0.04
+                        }
+                    ],
+                    "cons": []
+                },
+                {
+                    "name": "Flash Guard 5.56",
+                    "description": "Flash hider preserves vision and conceals position by reducing muzzle flash.",
+                    "pros": [
+                        {
+                            "tag": "increased_equipment_drop_rate"
+                        }
+                    ],
+                    "cons": []
+                },
+                {
+                    "name": "Suppressor",
+                    "description": "Reduces acoustic intensity of muzzle report and reduces muzzle flash.",
+                    "pros": [
+                        {
+                            "tag": "increased_equipment_drop_rate",
+                        }
+                    ],
+                    "cons": [
+                        {
+                            "tag": "effective_damage_range",
+                            "percent": 0.11
+                        },
+                        {
+                            "tag": "bullet_velocity",
+                            "percent": 0.08
+                        }
+                    ]
+                }
+            ],
+            "barrels": [
+                {
+                    "name": "11.8\" Extended",
+                    "description": "Hammer forged from chromoly steel to improve bullet velocity.",
+                    "pros": [
+                        {
+                            "tag": "bullet_velocity",
+                            "percent": 0.30
+                        }
+                    ],
+                    "cons": []
+                }
+            ],
+            "body": [],
+            "underbarrel": [],
+            "magazine": [],
+            "handle": [],
+            "stock": []
+        }
     },
     skills: {
         weapon_classes: [
